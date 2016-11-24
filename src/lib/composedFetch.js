cce345cb70d4ea97acf4cd5f499c7fc63b924ebb
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-11-24 13:16:18 +0100

    :ambulance: Fix return of fetch response API response now uses a top level key items followed by the array

--- src/lib/composedFetch.js
+++ src/lib/composedFetch.js
@@ -4,4 +4,4 @@ export default itemIdentifier => axios.get(`items`, {
   baseURL: `http://track.bpost.be/btr/api/`,
   responseType: `json`,
   params: { itemIdentifier },
-}).then(res => res.data[0]);
+}).then(res => res.data.items[0]);
