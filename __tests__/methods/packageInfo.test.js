5a26dae6568f1765a2cd66d36285f940862761f9
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-11-28 21:20:38 +0100

    :white_check_mark: Completed tests for all methods  #9

--- __tests__/methods/packageInfo.test.js
+++ __tests__/methods/packageInfo.test.js
@@ -1,20 +1,20 @@
 import * as bpost from '../../src/lib/methods/packageInfo';
 
-// describe(`Method: getPackageWeight`, () => {
-//   it(`should return the package weight in grams`, async done => {
-//     const request = await bpost.getPackageWeight(global.BPOST_ID);
-//     expect(request).toEqual(760);
-//     done();
-//   });
-// });
-//
-// describe(`Method: identifier`, () => {
-//   it(`should return the internal identifier`, async done => {
-//     const request = await bpost.identifier(global.BPOST_ID);
-//     expect(request).toEqual(759682715);
-//     done();
-//   });
-// });
+describe(`Method: getPackageWeight`, () => {
+  it(`should return the package weight in grams`, async done => {
+    const request = await bpost.getPackageWeight(global.BPOST_ID);
+    expect(request).toEqual(760);
+    done();
+  });
+});
+
+describe(`Method: identifier`, () => {
+  it(`should return the internal identifier`, async done => {
+    const request = await bpost.identifier(global.BPOST_ID);
+    expect(request).toEqual(759682715);
+    done();
+  });
+});
 
 describe(`Method: deliveryMethod`, () => {
   it(`should return requested delivery method`, async done => {
