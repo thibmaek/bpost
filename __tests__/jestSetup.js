af99a552d18370f24b29a8fa99476053fddc3f36
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-11-30 13:01:38 +0100

    :white_check_mark: Add test for homeDeliveryOptions and new methods

--- __tests__/jestSetup.js
+++ __tests__/jestSetup.js
@@ -1,6 +1,7 @@
 // Preset some stuff for async data flow
 beforeAll(done => {
-  global.BPOST_ID = process.env.npm_package_jest_globals_ID;
+  global.BPOST_ID_POSTAL = process.env.npm_package_jest_globals_ID_POSTAL;
+  global.BPOST_ID_HOME = process.env.npm_package_jest_globals_ID_HOME;
   global.BPOST_ID_FALSE = process.env.npm_package_jest_globals_ID_FALSE;
   global.jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
   done();
@@ -8,7 +9,8 @@ beforeAll(done => {
 
 // Reset the things we changed
 afterAll(() => {
-  global.BPOST_ID = undefined;
+  global.BPOST_ID_POSTAL = undefined;
+  global.BPOST_ID_HOME = undefined;
   global.BPOST_ID_FALSE = undefined;
   global.jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
 });
--- __tests__/methods/packageInfo.test.js
+++ __tests__/methods/packageInfo.test.js
@@ -2,7 +2,7 @@ import * as bpost from '../../src/lib/methods/packageInfo';
 
 describe(`Method: getPackageWeight`, () => {
   it(`should return the package weight in grams`, async done => {
-    const request = await bpost.getPackageWeight(global.BPOST_ID);
+    const request = await bpost.getPackageWeight(global.BPOST_ID_POSTAL);
     expect(request).toEqual(760);
     done();
   });
@@ -10,7 +10,7 @@ describe(`Method: getPackageWeight`, () => {
 
 describe(`Method: identifier`, () => {
   it(`should return the internal identifier`, async done => {
-    const request = await bpost.identifier(global.BPOST_ID);
+    const request = await bpost.identifier(global.BPOST_ID_POSTAL);
     expect(request).toEqual(759682715);
     done();
   });
@@ -18,8 +18,30 @@ describe(`Method: identifier`, () => {
 
 describe(`Method: deliveryMethod`, () => {
   it(`should return requested delivery method`, async done => {
-    const request = await bpost.deliveryMethod(global.BPOST_ID);
+    const request = await bpost.deliveryMethod(global.BPOST_ID_POSTAL);
     expect(request).toEqual([`postal`, `point`]);
+    done();
+  });
+});
+
+describe(`Method: hasRetourOptions`, () => {
+  it(`should return true or false according if retour is available`, async done => {
+    const request = await bpost.hasRetourOptions(global.BPOST_ID_POSTAL);
+    expect(request).toBeFalsy();
+    done();
+  });
+});
+
+describe(`Method: receiver`, () => {
+  it(`should return the object for with receiver data`, async done => {
+    const request = await bpost.receiver(global.BPOST_ID_POSTAL);
+    expect(request).toMatchSnapshot();
+    done();
+  });
+
+  it(`should return a formatted string is last argument is true`, async done => {
+    const request = await bpost.receiver(global.BPOST_ID_POSTAL, true);
+    expect(request).toEqual(`THIBAULT MAEKELBERGH, 8000 BRUGGE, BE`);
     done();
   });
 });
